68e8e61a8b2856d48919db4419e36ecf6ff524fe
LucasKonrath <lucaskdamaceno@gmail.com>
2025-06-07 09:39:04 -0700

    add optimized two-sum implementation with hashmap and update main for testing

--- src/two-sum.zig
+++ src/two-sum.zig
@@ -11,11 +11,38 @@ fn twoSumDumbSolution(nums: []const i32, target: i32) ?[2]usize {
     return null;
 }
 
+fn twoSumSmartSolution(nums: []const i32, target: i32) ? [2]usize {
+
+    // Create an allocator
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var map = std.AutoHashMap(i32, usize).init(allocator);
+    // Remember to free the map's memory when done
+    defer map.deinit();
+
+    for (nums, 0..) |num, index| {
+        if (map.get(target - num)) |value| {
+            return .{ value, index };
+        }
+        map.put(num, index) catch return null;
+    }
+
+    return null;
+}
+
 pub fn main() !void {
     const arr = [_]i32{ 2, 7, 11, 15 };
     if (twoSumDumbSolution(&arr, 9)) |indices| {
-        std.debug.print("Found indices: {any}\n", .{indices});
+        std.debug.print("Found indices duumb: {any}\n", .{indices});
     } else {
-        std.debug.print("No solution found\n", .{});
+        std.debug.print("No solution found dumb\n", .{});
+    }
+
+    if (twoSumSmartSolution(&arr, 9)) |indices| {
+        std.debug.print("Found indices smart: {any}\n", .{indices});
+    } else {
+        std.debug.print("No solution found smart\n", .{});
     }
 }
